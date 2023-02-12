59246ccffb37612c9d67451f7365d4c7632d96ea
bunesk <mail@bunesk.net>
2023-02-12 15:26:05 +0100

    file-based routing via vite-plugin-pages

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -1,22 +1,9 @@
 import {createRouter, createWebHistory} from 'vue-router';
-import Error404 from '../templates/Error404.vue';
+// @ts-ignore
+import routes from '~pages';
 
 export const install = (app: any) => {
-  // Auto generates routes from vue files under ./views
-  const views = import.meta.glob('../views/*.vue');
-
-  const generatedRoutes = Object.keys(views).map((path: any) => {
-    const name = path.match(/\.\/views(.*)\.vue$/)[1].toLowerCase();
-    return {
-      name: name.substring(1),
-      path: name === '/home' ? '/' : name,
-      component: views[path], // () => import('./views/*.vue')
-    };
-  });
-
-  const routes = [...generatedRoutes, {name: 'error404', path: '/:pathMatch(.*)*', component: Error404}];
-
-  // create router instance and pass the `routes` option
+  // create router instance and pass the automatically generated routes
   const router = createRouter({
     history: createWebHistory(),
     routes,
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {fileURLToPath, URL} from 'url';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
+import Pages from 'vite-plugin-pages';
 
 export default {
   resolve: {
@@ -13,5 +14,7 @@ export default {
     vue({}),
     // unplugin-vue-components
     Components({dts: 'src/types/components.d.ts'}),
+    // vite-plugin-pages
+    Pages({dirs: 'src/views'}),
   ],
 };
